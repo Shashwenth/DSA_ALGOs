0c1ab6ec0aa9226800cc09f1839a784807341bd9
Shashwenth <64272462+Shashwenth@users.noreply.github.com>
2024-08-28 12:17:20 -0400

    Updated Trie
    Included function to find words contains a prefix 'p'

--- Trie/src/main_/Runner.java
+++ Trie/src/main_/Runner.java
@@ -1,6 +1,7 @@
 package main_;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import nodeStructure.Node;
 import operations.Dictionary;
@@ -11,6 +12,10 @@ public class Runner {
 
 	public static void main(String args[]) {
 		
+		Runtime runtime = Runtime.getRuntime();
+		long memoryBefore = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Used Memory in bytes: " + memoryBefore);
+		
 		
 		System.out.println("Creating Node");
 		Node root=new Node(null, false, new ArrayList<Node>());
@@ -20,6 +25,7 @@ public class Runner {
 		Insert iTrie=new Insert();
 		iTrie.insert(root, "Shash", 0);
 		iTrie.insert(root, "Shashwenth", 0);
+		iTrie.insert(root, "Shashii", 0);
 		iTrie.insert(root, "Muralidharan", 0);
 		iTrie.insert(root, "     ", 0); //5 spaces
 		iTrie.insert(root, "      !@", 0); //5 spaces
@@ -34,11 +40,28 @@ public class Runner {
 		System.out.println(dTrie.find(root, "Shash", 0)); //True
 		System.out.println(dTrie.find(root, "Shashi", 0));  //False
 		System.out.println(dTrie.find(root, "Shashwenth", 0)); //True
+		System.out.println(dTrie.find(root, "ShashwenthShashwenth", 0)); //False
 		System.out.println(dTrie.find(root, "Muralidharan", 0));  //True
 		System.out.println(dTrie.find(root, "Shashwenthii", 0));  //False
 		System.out.println(dTrie.find(root, "Muralidharan ", 0));  //False
 		System.out.println(dTrie.find(root, "     ", 0)); //True
 		System.out.println(dTrie.find(root, "     !", 0));  //False
+		System.out.println(dTrie.find(root, "A", 0));  //False
+		
+		//List Prefixes
+		System.out.println("List prefixes for Sh");
+		List<String> list=new ArrayList<>();
+		dTrie.find_prefix(root,"Sh" , list, 0);
+		System.out.println(list);
+		
+		
+		long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Used Memory in bytes after execution: " + memoryAfter);
+
+        // Memory used by your code
+        long memoryUsed = memoryAfter - memoryBefore;
+        System.out.println("Memory used by your code in bytes: " + memoryUsed);
+	
 		
 		
 		
--- Trie/src/operations/Dictionary.java
+++ Trie/src/operations/Dictionary.java
@@ -1,5 +1,7 @@
 package operations;
 
+import java.util.List;
+
 import nodeStructure.Node;
 
 public class Dictionary {
@@ -19,14 +21,50 @@ public class Dictionary {
 			}
 			return false;
 		}
+		//boolean ans=false;
 		for(Node node:root.children) {
 			if(node.value==str.charAt(i)) {
+				//ans = ans || find(node, str, i+1);
 				return find(node, str, i+1);
 			}
 		}
+		//return ans;
 		return false;
 		
 	}
+	
+	public void find_prefix(Node root, String prefix, List<String> list, int i){
+		if(i==prefix.length()) {
+			fillPrefix(root, list, prefix);
+			return;
+		}
+		if(root==null) {
+			return;
+		}
+		
+		for(Node node: root.children) {
+			if(node.value==prefix.charAt(i)) {
+				find_prefix(node, prefix, list, i+1);
+			}
+		}
+		
+	}
+
+	private void fillPrefix(Node root, List<String> list, String curr) {
+		
+		if(root==null) {
+			return;
+		}
+		if(root.isEnd) {
+			list.add(curr);
+		}
+		for(Node node:root.children) {
+			fillPrefix(node, list, curr+String.valueOf(node.value));
+		}
+		
+	}
+	
+	
 
 	
 	
